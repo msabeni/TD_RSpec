953f51b5cd3ce44468cdc02cb1f7889e015c0da8
msabeni <kemimsabeni@gmail.com>
2021-01-11 21:10:19 +0300

    indntation

--- dog.rb
+++ dog.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Dog
-    def bark
-        "woof!"
-    end
+  def bark
+    'woof!'
+  end
 end
--- dog_spec.rb
+++ dog_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 require 'rspec'
 
 require_relative 'dog'
 
 describe Dog do
-    describe '#bark' do
-        it 'returns the "woof!"' do
-            dog = Dog.new 
-            expect(dog.bark).to eql("woof!")  
-            end
-        end
-            
+  describe '#bark' do
+    it 'returns the "woof!"' do
+      dog = Dog.new
+      expect(dog.bark).to eql('woof!')
+    end
+  end
 end
